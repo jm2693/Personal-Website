0edfe565101b7bf83bb521e9fdcfe99dbc76e1a0
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-08-14 20:58:49 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Test Header</h1>
       </header>
+      <main className='App-main'>
+        <p>Jason Merchan</p>
+        <p>Software Engineer</p>
+      </main>
     </div>
   );
 }
---
+++ src/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './header.css';
+
+function Header() {
+    <Header className="App-Header">
+        <div className="Header-Content">
+            <div className="name-title">
+                <h1>Jason Merchan</h1>
+                <h2>Software Engineer</h2>
+                
+            </div>
+        </div>
+    </Header>
+}
+
+export default Header;
