be7fb7df9e5d7d06cc6d0a5580995eb02451f615
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-10-07 01:55:36 -0400

    working more about frames

--- src/components/moreAboutMe.jsx
+++ src/components/moreAboutMe.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const MoreAboutMe = () => {
     return (
-        <h2 className="text-4xl font-museomoderno font-bold mb-6">
-            More About Me
-            <div className="bg-transparent backdrop-blur-sm rounded-2xl p-6 shadow-md mb-6">
-            <div className={`text-xl font-museomoderno font-bold px-1 rounded-md inline-block`}>
-                <ul className="space-y-2 ml-6"></ul>
-            </div>
-            </div>
-        </h2>
+        <div>
+            <h2 className="text-4xl font-museomoderno font-bold mb-6">More About Me</h2>
+
+                <div className="bg-transparent backdrop-blur-sm rounded-2xl p-6 shadow-md mb-6">
+                    <div className="text-lg bg-primaryBrown bg-opacity-10 backdrop-blur-lg rounded-md p-2 shadow-sm">
+                        
+                    </div>
+                </div>
+        </div>
       );
 }
 
