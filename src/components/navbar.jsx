e9019b09d860ccf64c844fd48831845e911de861
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-11-02 15:47:01 -0400

    final correction

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
                 </a>
 
                 <a 
-                href="https://linkedin.com/jason-merchan"
+                href="https://www.linkedin.com/in/jason-merchan"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:opacity-80 transition-opacity"
